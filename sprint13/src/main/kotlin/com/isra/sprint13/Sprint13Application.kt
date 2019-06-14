51312bbe094a02e5c378dff7d53d895bc087330d
Israel dela Cruz <igdcruz42@gmail.com>
2019-06-14 10:07:42 -0500

    added project

---
+++ sprint13/src/main/kotlin/com/isra/sprint13/Sprint13Application.kt
@@ -0,0 +1,11 @@
+package com.isra.sprint13
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Sprint13Application
+
+fun main(args: Array<String>) {
+    runApplication<Sprint13Application>(*args)
+}
