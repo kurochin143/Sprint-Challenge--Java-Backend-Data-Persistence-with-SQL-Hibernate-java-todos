e59e999a8de66cb5ce9c45e639af2479ade8d930
Israel dela Cruz <igdcruz42@gmail.com>
2019-06-14 10:16:38 -0500

    added oauth2 service

---
+++ sprint13/src/main/kotlin/com/isra/sprint13/service/UserService.java
@@ -0,0 +1,18 @@
+package com.isra.sprint13.service;
+
+import com.isra.sprint13.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    User findUserById(long id);
+
+    void delete(long id);
+
+    User save(User user);
+
+    User update(User user, long id);
+}
